d0302ddbf5844f2ba743507b5f7b5a5efbb11707
PalmNiklas <60201549+PalmNiklas@users.noreply.github.com>
2020-05-27 13:14:59 +0200

    added profile login page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="profile">Profile Page</div>
+  <div class="profile">
+    <top class="top" />
+
+    <article class="login-form">
+      <header>
+        <div class="logo">
+          <img src="../assets/graphics/logo-black.svg" alt />
+        </div>
+
+        <h1>Välkommen till AirBean-familjen!</h1>
+        <h2>Genom att skapa ett konto nedan kan du spara och se din orderhistorik.</h2>
+      </header>
+      <div class="input-form">
+        <p class="name">Name</p>
+        <input type="text" class="name" />
+        <p class="email">Email</p>
+        <input type="text" class="name" />
+        <div class="gdpr-btn">
+          <input  type="checkbox" name="gdpr" id="gdpr" />
+          <label for="gdpr">GDPR ok!</label>
+        </div>
+        <br />
+        <button>Brew me a cup!</button>
+      </div>
+    </article>
+  </div>
 </template>
 <script>
+import Top from "../components/Top";
 export default {
+  Name: "Profile",
+  components: {
+    Top
+  },
   data: () => ({}),
 
   methods: {}
@@ -10,4 +40,88 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/_variables.scss";
+
+.profile {
+  background-color: $secondary-color;
+  color: $secondary-color;
+  height: 100vh;
+
+  .logo {
+    background-color: rgba(0, 0, 0, 0.2);
+    // padding: 0.1rem;
+    height: 2rem;
+    width: 2rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+    }
+  }
+
+  .login-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+    padding: 1rem;
+    background-color: $primary-color;
+    border-radius: 0.2rem;
+    box-shadow: 10px 10px 24px 0px rgba(0, 0, 0, 0.75);
+
+    input {
+      font-family: Work Sans;
+      background-color: $primary-color;
+      font-size: 1rem;
+      text-indent: 0.5rem;
+      height: 2.5rem;
+      border: 1px solid #2f2926;
+      box-sizing: border-box;
+      border-radius: 6px;
+    }
+
+    .gdpr-btn{
+      text-align: center;
+      display: flex;
+      align-items: center;
+      font-size: 0.8rem;
+    }
+
+    button {
+            font-family: PT Serif;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 24px;
+    }
+
+    p {
+      font-family: Work Sans;
+    }
+
+    header {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+
+      h1 {
+        font-family: PT Serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 2rem;
+        line-height: 120%;
+        margin: 0;
+      }
+      h2 {
+        font-family: Work Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 1rem;
+        line-height: 150%;
+      }
+    }
+  }
+}
 </style>
