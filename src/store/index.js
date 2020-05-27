5ede2befa330e0c51d70d0596161e68f66ad8280
r0bban <robert.p.svensson@gmail.com>
2020-05-27 10:05:23 +0200

    added coffe to stor

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import CoffeMenu from '@/assets/data/menu.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    menu: CoffeMenu.menu
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters:{
+    getAllProducts(state){
+      return state.menu;
+    }
   }
 })
